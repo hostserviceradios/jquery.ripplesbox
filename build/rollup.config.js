00bd25a7f1c46ec26e60868f2216a0a55c86d649
hostserviceradios <79023911+hostserviceradios@users.noreply.github.com>
2025-10-02 23:12:01 -0400

    Update rollup.config.js

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -3,9 +3,9 @@ import license from 'rollup-plugin-license'
 
 const version = require('../package.json').version
 const banner =
-`jQuery Ripples plugin v${version} / https://github.com/sirxemic/jquery.ripples
+`jQuery Ripples plugin v${version} / https://github.com/hostserviceradios/jquery.ripplesbox
 MIT License
-@author sirxemic / https://sirxemic.com/`
+@author sirxemic / https://sonybox.es/`
 
 export default {
   entry: path.join(__dirname, '../src/main.js'),
